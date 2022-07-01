e196c0e3400c194dc54ee6af85999c3a615b286a
manoharmedapu <87259044+manoharmedapu@users.noreply.github.com>
2022-07-01 13:03:50 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
+    console.log('devops ****')
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
